5de90d92dbb9688d130548b3f8323c9358cba20d
Rhys Campbell <rhys.james.campbell@googlemail.com>
2017-03-12 16:13:58 +0100

    Few additions

--- bash/cockroachdb_cluster.sh
+++ bash/cockroachdb_cluster.sh
@@ -34,5 +34,68 @@ cockroach start --background --cache=50M --store=./node5 --port=26261 --http-por
 # 	FAMILY "primary" (id, text)
 # )
 
+# Now let's see if all the nodes have correct data
+cockroach sql --port 26257 --execute "SELECT COUNT(*) FROM rhys.test";
+cockroach sql --port 26258 --execute "SELECT COUNT(*) FROM rhys.test";
+cockroach sql --port 26259 --execute "SELECT COUNT(*) FROM rhys.test";
+cockroach sql --port 26260 --execute "SELECT COUNT(*) FROM rhys.test";
+cockroach sql --port 26261 --execute "SELECT COUNT(*) FROM rhys.test";
+
+# Insert into different nodes
+cockroach sql --port 26257 --execute "INSERT INTO rhys.test (text) VALUES ('Node 1')";
+cockroach sql --port 26258 --execute "INSERT INTO rhys.test (text) VALUES ('Node 2')";
+cockroach sql --port 26259 --execute "INSERT INTO rhys.test (text) VALUES ('Node 3')";
+cockroach sql --port 26260 --execute "INSERT INTO rhys.test (text) VALUES ('Node 4')";
+cockroach sql --port 26261 --execute "INSERT INTO rhys.test (text) VALUES ('Node 5')";
+
+# Check counts again
+cockroach sql --port 26257 --execute "SELECT COUNT(*) FROM rhys.test";
+cockroach sql --port 26258 --execute "SELECT COUNT(*) FROM rhys.test";
+cockroach sql --port 26259 --execute "SELECT COUNT(*) FROM rhys.test";
+cockroach sql --port 26260 --execute "SELECT COUNT(*) FROM rhys.test";
+cockroach sql --port 26261 --execute "SELECT COUNT(*) FROM rhys.test";
+
+# Check data on a couple of nodes
+rhysmacbook:cockroach_cluster_tmp rhys1$ cockroach sql --port 26261 --execute "SELECT * FROM rhys.test";
++--------------------+-------------+
+|         id         |    text     |
++--------------------+-------------+
+| 226950927534555137 | Hello World |
+| 226951064182259713 | Hello World |
+| 226951080098856961 | Hello World |
+| 226952456016003073 | Node 1      |
+| 226952456149368834 | Node 2      |
+| 226952456292663299 | Node 3      |
+| 226952456455684100 | Node 4      |
+| 226952456591376389 | Node 5      |
++--------------------+-------------+
+(8 rows)
+rhysmacbook:cockroach_cluster_tmp rhys1$ cockroach sql --port 26260 --execute "SELECT * FROM rhys.test";
++--------------------+-------------+
+|         id         |    text     |
++--------------------+-------------+
+| 226950927534555137 | Hello World |
+| 226951064182259713 | Hello World |
+| 226951080098856961 | Hello World |
+| 226952456016003073 | Node 1      |
+| 226952456149368834 | Node 2      |
+| 226952456292663299 | Node 3      |
+| 226952456455684100 | Node 4      |
+| 226952456591376389 | Node 5      |
++--------------------+-------------+
+(8 rows)
+
+# clean up (gets rid of all process and data!)
+cockroach quit --port=26257
+cockroach quit --port=26258
+cockroach quit --port=26259
+cockroach quit --port=26260
+cockroach quit --port=26261
+cd;
+rm -Rf cockroach_cluster_tmp;
+
+# TODO
+# Authentication & Encryption https://www.cockroachlabs.com/docs/secure-a-cluster.html
+
 
 

19ca5c3643c0b20408ac9f91448642c991b30c42
Rhys Campbell <rhys.james.campbell@googlemail.com>
2017-03-18 16:53:18 +0100

    Organised bash into functions

--- bash/cockroachdb_cluster.sh
+++ bash/cockroachdb_cluster.sh
@@ -1,23 +1,66 @@
-# Download and set PATH
-wget https://binaries.cockroachdb.com/cockroach-latest.darwin-10.9-amd64.tgz
-tar xvzf cockroach-latest.darwin-10.9-amd64.tgz
-PATH="$PATH:/Users/rhys1/cockroach-latest.darwin-10.9-amd64";
-export PATH;
+# Download and set PATH if needed
+cc_dl_set_path()
+{
+	command -v cockroach >/dev/null 2>&1 || {
+		cd;
+		wget https://binaries.cockroachdb.com/cockroach-latest.darwin-10.9-amd64.tgz
+		tar xvzf cockroach-latest.darwin-10.9-amd64.tgz
+		PATH="$PATH:/Users/rhys1/cockroach-latest.darwin-10.9-amd64";
+		export PATH;
+	}
+}
 
 # Setup the cluster nodes
-mkdir -p cockroach_cluster_tmp/node1;
-mkdir -p cockroach_cluster_tmp/node2;
-mkdir -p cockroach_cluster_tmp/node3;
-mkdir -p cockroach_cluster_tmp/node4;
-mkdir -p cockroach_cluster_tmp/node5;
-cd cockroach_cluster_tmp
-cockroach start --background --cache=50M --store=./node1;
-cockroach start --background --cache=50M --store=./node2 --port=26258 --http-port=8081 --join=localhost:26257;
-cockroach start --background --cache=50M --store=./node3 --port=26259 --http-port=8082 --join=localhost:26257;
-cockroach start --background --cache=50M --store=./node4 --port=26260 --http-port=8083 --join=localhost:26257;
-cockroach start --background --cache=50M --store=./node5 --port=26261 --http-port=8084 --join=localhost:26257;
+function cc_mkdirs()
+{
+	mkdir -p cockroach_cluster_tmp/node1;
+	mkdir -p cockroach_cluster_tmp/node2;
+	mkdir -p cockroach_cluster_tmp/node3;
+	mkdir -p cockroach_cluster_tmp/node4;
+	mkdir -p cockroach_cluster_tmp/node5;
+}
 
+function cc_start_cluster_nodes()
+{
+	cd cockroach_cluster_tmp
+	cockroach start --background --cache=50M --store=./node1;
+	cockroach start --background --cache=50M --store=./node2 --port=26258 --http-port=8081 --join=localhost:26257;
+	cockroach start --background --cache=50M --store=./node3 --port=26259 --http-port=8082 --join=localhost:26257;
+	cockroach start --background --cache=50M --store=./node4 --port=26260 --http-port=8083 --join=localhost:26257;
+	cockroach start --background --cache=50M --store=./node5 --port=26261 --http-port=8084 --join=localhost:26257;
+}
+
+# Needs xdg-utils install, OS X - sudo port install xdg-utils
 # Access web console at http://localhost:8084
+function cc_web_console()
+{
+	command -v xdg-open >/dev/null 2>&1 xdg-open && xdg-open http://localhost:8084 || echo "Problem laucnhing browser. Is xdg-open installed?";
+}
+
+# clean up (gets rid of all processes and data!)
+function cc_murder_cluster()
+{
+	pgrep -x cockroach && {
+		cockroach quit --port=26257
+		cockroach quit --port=26258
+		cockroach quit --port=26259
+		cockroach quit --port=26260
+		cockroach quit --port=26261
+	}
+	cd;
+	rm -Rf cockroach_cluster_tmp;
+}
+
+function cc_setup_cluster()
+{
+	cc_dl_set_path;
+	cc_mkdirs;
+	cc_start_cluster_nodes;
+	cc_web_console;
+}
+
+# TODO
+# Authentication & Encryption https://www.cockroachlabs.com/docs/secure-a-cluster.html	
 
 # Command-line access
 # cockroach sql;
@@ -35,67 +78,57 @@ cockroach start --background --cache=50M --store=./node5 --port=26261 --http-por
 # )
 
 # Now let's see if all the nodes have correct data
-cockroach sql --port 26257 --execute "SELECT COUNT(*) FROM rhys.test";
-cockroach sql --port 26258 --execute "SELECT COUNT(*) FROM rhys.test";
-cockroach sql --port 26259 --execute "SELECT COUNT(*) FROM rhys.test";
-cockroach sql --port 26260 --execute "SELECT COUNT(*) FROM rhys.test";
-cockroach sql --port 26261 --execute "SELECT COUNT(*) FROM rhys.test";
+# cockroach sql --port 26257 --execute "SELECT COUNT(*) FROM rhys.test";
+# cockroach sql --port 26258 --execute "SELECT COUNT(*) FROM rhys.test";
+# cockroach sql --port 26259 --execute "SELECT COUNT(*) FROM rhys.test";
+# cockroach sql --port 26260 --execute "SELECT COUNT(*) FROM rhys.test";
+# cockroach sql --port 26261 --execute "SELECT COUNT(*) FROM rhys.test";
 
 # Insert into different nodes
-cockroach sql --port 26257 --execute "INSERT INTO rhys.test (text) VALUES ('Node 1')";
-cockroach sql --port 26258 --execute "INSERT INTO rhys.test (text) VALUES ('Node 2')";
-cockroach sql --port 26259 --execute "INSERT INTO rhys.test (text) VALUES ('Node 3')";
-cockroach sql --port 26260 --execute "INSERT INTO rhys.test (text) VALUES ('Node 4')";
-cockroach sql --port 26261 --execute "INSERT INTO rhys.test (text) VALUES ('Node 5')";
+# cockroach sql --port 26257 --execute "INSERT INTO rhys.test (text) VALUES ('Node 1')";
+# cockroach sql --port 26258 --execute "INSERT INTO rhys.test (text) VALUES ('Node 2')";
+# cockroach sql --port 26259 --execute "INSERT INTO rhys.test (text) VALUES ('Node 3')";
+# cockroach sql --port 26260 --execute "INSERT INTO rhys.test (text) VALUES ('Node 4')";
+# cockroach sql --port 26261 --execute "INSERT INTO rhys.test (text) VALUES ('Node 5')";
 
 # Check counts again
-cockroach sql --port 26257 --execute "SELECT COUNT(*) FROM rhys.test";
-cockroach sql --port 26258 --execute "SELECT COUNT(*) FROM rhys.test";
-cockroach sql --port 26259 --execute "SELECT COUNT(*) FROM rhys.test";
-cockroach sql --port 26260 --execute "SELECT COUNT(*) FROM rhys.test";
-cockroach sql --port 26261 --execute "SELECT COUNT(*) FROM rhys.test";
+# cockroach sql --port 26257 --execute "SELECT COUNT(*) FROM rhys.test";
+# cockroach sql --port 26258 --execute "SELECT COUNT(*) FROM rhys.test";
+# cockroach sql --port 26259 --execute "SELECT COUNT(*) FROM rhys.test";
+# cockroach sql --port 26260 --execute "SELECT COUNT(*) FROM rhys.test";
+# cockroach sql --port 26261 --execute "SELECT COUNT(*) FROM rhys.test";
 
 # Check data on a couple of nodes
-rhysmacbook:cockroach_cluster_tmp rhys1$ cockroach sql --port 26261 --execute "SELECT * FROM rhys.test";
-+--------------------+-------------+
-|         id         |    text     |
-+--------------------+-------------+
-| 226950927534555137 | Hello World |
-| 226951064182259713 | Hello World |
-| 226951080098856961 | Hello World |
-| 226952456016003073 | Node 1      |
-| 226952456149368834 | Node 2      |
-| 226952456292663299 | Node 3      |
-| 226952456455684100 | Node 4      |
-| 226952456591376389 | Node 5      |
-+--------------------+-------------+
-(8 rows)
-rhysmacbook:cockroach_cluster_tmp rhys1$ cockroach sql --port 26260 --execute "SELECT * FROM rhys.test";
-+--------------------+-------------+
-|         id         |    text     |
-+--------------------+-------------+
-| 226950927534555137 | Hello World |
-| 226951064182259713 | Hello World |
-| 226951080098856961 | Hello World |
-| 226952456016003073 | Node 1      |
-| 226952456149368834 | Node 2      |
-| 226952456292663299 | Node 3      |
-| 226952456455684100 | Node 4      |
-| 226952456591376389 | Node 5      |
-+--------------------+-------------+
-(8 rows)
+# rhysmacbook:cockroach_cluster_tmp rhys1$ cockroach sql --port 26261 --execute "SELECT * FROM rhys.test";
+#+--------------------+-------------+
+#|         id         |    text     |
+#+--------------------+-------------+
+#| 226950927534555137 | Hello World |
+#| 226951064182259713 | Hello World |
+#| 226951080098856961 | Hello World |
+#| 226952456016003073 | Node 1      |
+#| 226952456149368834 | Node 2      |
+#| 226952456292663299 | Node 3      |
+#| 226952456455684100 | Node 4      |
+#| 226952456591376389 | Node 5      |
+#+--------------------+-------------+
+#(8 rows)
+#rhysmacbook:cockroach_cluster_tmp rhys1$ cockroach sql --port 26260 --execute "SELECT * FROM rhys.test";
+#+--------------------+-------------+
+#|         id         |    text     |
+#+--------------------+-------------+
+#| 226950927534555137 | Hello World |
+#| 226951064182259713 | Hello World |
+#| 226951080098856961 | Hello World |
+#| 226952456016003073 | Node 1      |
+#| 226952456149368834 | Node 2      |
+#| 226952456292663299 | Node 3      |
+#| 226952456455684100 | Node 4      |
+#| 226952456591376389 | Node 5      |
+#+--------------------+-------------+
+#(8 rows)
 
-# clean up (gets rid of all processes and data!)
-cockroach quit --port=26257
-cockroach quit --port=26258
-cockroach quit --port=26259
-cockroach quit --port=26260
-cockroach quit --port=26261
-cd;
-rm -Rf cockroach_cluster_tmp;
 
-# TODO
-# Authentication & Encryption https://www.cockroachlabs.com/docs/secure-a-cluster.html
 
 
 
